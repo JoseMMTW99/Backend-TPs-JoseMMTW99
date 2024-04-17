c1698f49d2e8b82b9432e85495ab0188890500e4
José María Martínez Terán Weht <josemtw@MacBook-Air-de-Jose.local>
2024-04-17 20:21:14 -0300

    correcion archivos

--- desafio_4/src/public/js/main.realTimeProducts.js
+++ desafio_4/src/public/js/main.realTimeProducts.js
@@ -2,10 +2,10 @@ const socket = io();
 const content = document.getElementById("content");
 
 socket.on("realTimeProducts", (data) => {
-    let salida = ``;
+  let salida = ``;
 
-    data.forEach(item => {
-        salida += `<div class="col-md-4">
+  data.forEach((item) => {
+    salida += `<div class="col-md-4">
             <div class="card border-0 mb-3">
                 <img src="${item.thumbnails}" class="img-fluid" alt="${item.title}">
                 <div class="card-body text-center">
@@ -13,28 +13,28 @@ socket.on("realTimeProducts", (data) => {
                 </div>
             </div>
         </div>`;
-    });
+  });
 
-    content.innerHTML = salida;
+  content.innerHTML = salida;
 });
 
 const agregarProducto = () => {
-    const title = document.getElementById("title").value;
-    const thumbnails = document.getElementById("thumbnails").value;
-    const price = document.getElementById("price").value;
-    const product = {title:title, thumbnails:thumbnails, price:price};
+  const title = document.getElementById("title").value;
+  const thumbnails = document.getElementById("thumbnails").value;
+  const price = document.getElementById("price").value;
+  const product = { title: title, thumbnails: thumbnails, price: price };
 
-    socket.emit("nuevoProducto", product);
-}
+  socket.emit("nuevoProducto", product);
+};
 
 const btnAgregarProducto = document.getElementById("btnAgregarProducto");
 btnAgregarProducto.onclick = agregarProducto;
 
 const eliminarProducto = () => {
-    const idProduct = document.getElementById("idProduct").value;
+  const idProduct = document.getElementById("idProduct").value;
 
-    socket.emit("eliminarProducto", idProduct);
-}
+  socket.emit("eliminarProducto", idProduct);
+};
 
 const btnEliminarProducto = document.getElementById("btnEliminarProducto");
 btnEliminarProducto.onclick = eliminarProducto;
